4d9b7185efb83441954b83f907c3802dda69ff1a
Mustafa <imocuk@gmail.com>
2022-12-13 22:18:29 +0200

    Update

--- voter/screens/main_screen.py
+++ voter/screens/main_screen.py
@@ -1,6 +1,11 @@
 import self
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
+from kivymd.icon_definitions import md_icons
+from kivymd.toast import toast
+from kivymd.uix.card import MDCardSwipe
+from kivymd.uix.list import OneLineIconListItem, OneLineListItem, TwoLineListItem
 
 screen_str = """
 <MainScreen>
@@ -16,13 +21,36 @@ screen_str = """
                 text: "0"
                 pos_hint:{'center_x': 0.27, 'center_y': .97}
                 
-        RecycleBoxLayout:
-            id: rv
-            default_size: None, dp(56)
-            default_size_hint: 1, None
-            size_hint_y: None
-            height: self.minimum_height
-            orientation: 'vertical'
+            ScrollView:
+                pos_hint:{'center_y': .44}
+                MDList:
+                    id: md_list_container
+                    OneLineListItem:
+                        text: "------- Adaylar -------"
+                    TwoLineListItem:
+                        id: "1"
+                        text: "1"
+                        secondary_text: "Kara Parti"
+                        on_release: root._oy_ver(self, "1")
+                    TwoLineListItem:
+                        id: "2"
+                        text: "2"
+                        secondary_text: "Halki Soy Partisi"
+                        on_release: root._oy_ver(self, "2")
+                    TwoLineListItem:
+                        id: "3"
+                        text: "3"
+                        secondary_text: "Halk Fakir Biz Zengin Parti"
+                        on_release: root._oy_ver(self, "3")
+                    TwoLineListItem:
+                        id: "4"
+                        text: "4"
+                        secondary_text: "Politikacilar Kraldir Partisi"
+                        on_release: root._oy_ver(self, "4")
+                    OneLineListItem:
+                        text: "###########################"
+                    
+                    
 """
 
 
@@ -30,10 +58,18 @@ class MainScreen(Screen):
     Builder.load_string(screen_str)
 
     def on_enter(self, *args):
-        pass
+        self.ids.label_oy_sayisi.text = self._get_oy_sayisi()
 
     def _get_oy_sayisi(self):
-        pass
+        # Bu veri municipality'den gelmeli
+        oy_sayisi = 11
+        return str(oy_sayisi)
+
+    def _oy_ver(self, pressed, list_id):
+        print(pressed.text, list_id)
+        print("SC text :" + pressed.secondary_text,  "id :"+list_id)
+        toast("Oy verildi")
+
     def on_pre_enter(self, *args):
         if self.manager.get_screen("LoginScreen").password_check():
             pass
